694db2e469386fbd1a9e57be6dba66aec9dbf8f4
Erwan <erwan.a.marechal@gmail.com>
2025-11-26 10:20:06 +0100

    feat: implement user authentication and favorites management with MongoDB

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,24 @@
+import { MongoClient, type MongoClientOptions } from 'mongodb';
+
+const uri = import.meta.env.MONGODB_URI;
+const options: MongoClientOptions = {};
+
+let client: MongoClient | undefined;
+let clientPromise: Promise<MongoClient>;
+
+if (!uri) {
+  throw new Error('Please add your MongoDB URI to the .env file');
+}
+
+if (process.env.NODE_ENV === 'development') {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
